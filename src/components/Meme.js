88de09476c7ba53fbb407921456d68304a5f2d17
jstone <jstonedev521@gmail.com>
2022-02-04 21:22:16 -0500

    update form logic

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { MemesData } from "../memesData";
 import "./meme.css";
 
 const Meme = () => {
+	const [meme, setMeme] = useState({
+		topText: "",
+		bottomText: "",
+		randomImage: "http://i.imgflip.com/1bij.jpg",
+	});
+	const [allMemeImages] = useState(MemesData);
+
+	const getMemeImage = () => {
+		const memesArray = allMemeImages.data.memes;
+		const randIdx = Math.floor(Math.random() * memesArray.length);
+		const url = memesArray[randIdx].url;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			randomImage: url,
+		}));
+		console.log(meme);
+	};
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			[name]: value,
+		}));
+	};
+
 	return (
 		<main>
-			<form className="form">
-				<input className="form--input" type="text" placeholder="top text" />
-				<input className="form--input" type="text" placeholder="bottom text" />
-				<button type="submit" className="form--btn">
+			<div className="form">
+				<input
+					type="text"
+					className="form--input"
+					placeholder="Top text"
+					name="topText"
+					value={meme.topText}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					className="form--input"
+					placeholder="Bottom text"
+					name="bottomText"
+					value={meme.bottomText}
+					onChange={handleChange}
+				/>
+				<button className="form--btn" onClick={getMemeImage}>
 					Get a new meme image 🖼
 				</button>
-			</form>
+			</div>
+			<div className="meme">
+				<img className="meme-img" src={meme.randomImage} alt="meme" />
+				<h2 className="meme--text top">{meme.topText}</h2>
+				<h2 className="meme--text bottom">{meme.bottomText}</h2>
+			</div>
 		</main>
 	);
 };
